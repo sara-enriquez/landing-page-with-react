883013c546a063dd47c5124ab8c99a799e1cc94a
Sara Enríquez <sara_enriquezdesalamanca@hotmail.com>
2023-02-25 19:13:13 +0000

    removed comments

--- src/js/component/container.jsx
+++ src/js/component/container.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Jumbotron from "./jumbotron.jsx";
 import Cards from "./cards.jsx";
 
-//create your first component
+
 const Container = () => {
   return (
     <div className="container">
--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-//create your first component
 const Footer = () => {
   return (
     <div>
